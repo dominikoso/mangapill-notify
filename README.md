29f55d1b4787a1ed27d6fa2d11359000c8aa454b
dominikoso <mcdominikoso@gmail.com>
2021-04-22 17:28:26 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mangapill-notify
 Simple Notification System for new Manga Chapters using telegram-chat
+
+# How to use
+1. Create bot using https://telegram.me/BotFather
+2. Linux Setup
+```
+$ pip3 install -r requirements.txt
+$ telegram-chat --configure
+$ python3 ./main.py
+```
--- main.py
+++ main.py
@@ -21,10 +21,12 @@ while True:
     chapter = html.find('a', class_='py-1 px-2 border border-color-border-secondary rounded text-sm')
     chapter_name = list(chapter.children)[1].text
     chapter_link = 'https://mangapill.com' + chapter['href']
+
     if (last_chapter == 0) or (last_chapter != chapter_name):
         last_chapter = chapter_name
         message = '[!] New Black Clover Chapter is available: \n- {} \n- {}'.format(chapter_name, chapter_link)
         telegram_send.send(messages=[message])
     else:
         log.info('[{}] No new chapter found'.format(dt_string))
+        
     time.sleep(3600)
